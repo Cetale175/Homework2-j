5c4c658eb550bb6d5ca51591a81c5c8f20b59128
Kirylbas <kirill.kolobov.98@gmail.com>
2023-01-26 21:15:20 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 5175;
+        int b = 20;
+
+        System.out.println("Бонус: " + ticket / b );
+
+
+    }
+
+}
